dca8a47807408d2e7b69b272ef5684b7cf419c44
mathwiz <yohanlee@yahoo.com>
2018-02-26 15:16:48 -0500

    Hello World example

--- Cpp.TDD/HelloWorld.cpp
+++ Cpp.TDD/HelloWorld.cpp
@@ -1,13 +1,15 @@
 //
-//  main.cpp
+//  HelloWorld.cpp
 //  Cpp.TDD
 //
 //  Created by Yohan Lee on 2/26/18.
 //  Copyright © 2018 Yohan Lee. All rights reserved.
 //
+#include <iostream>
 
 #include "HelloWorld.hpp"
 
-int main(int argc, const char * argv[]) {
-    return hello();
+int hello() {
+    std::cout << "Hello, Sucker!\n";
+    return 0;
 }
---
+++ Cpp.TDD/HelloWorld.hpp
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.hpp
+//  Cpp.TDD
+//
+//  Created by Yohan Lee on 2/26/18.
+//  Copyright © 2018 Yohan Lee. All rights reserved.
+//
+
+#ifndef HelloWorld_hpp
+#define HelloWorld_hpp
+
+#include <stdio.h>
+int hello();
+
+
+#endif /* HelloWorld_hpp */
