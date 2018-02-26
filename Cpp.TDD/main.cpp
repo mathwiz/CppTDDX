54ba9a912a9324a340e12d0d9b77951d9eaade1c
mathwiz <yohanlee@yahoo.com>
2018-02-26 15:07:40 -0500

    Initial Commit

---
+++ Cpp.TDD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp.TDD
+//
+//  Created by Yohan Lee on 2/26/18.
+//  Copyright © 2018 Yohan Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
