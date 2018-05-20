0b2aa782b0f16b36851a4d829ce295646d245a29
mathwiz <yohanlee@yahoo.com>
2018-05-20 14:01:59 -0400

    New target for KR code

---
+++ KR/kr1.h
@@ -0,0 +1,16 @@
+//
+//  kr1.h
+//  Cpp.TDD
+//
+//  Created by Yohan Lee on 5/20/18.
+//  Copyright © 2018 Yohan Lee. All rights reserved.
+//
+
+#ifndef kr1_h
+#define kr1_h
+
+#include <stdio.h>
+
+int hello_world();
+
+#endif /* kr1_h */
---
+++ KR/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  KR
+//
+//  Created by Yohan Lee on 5/20/18.
+//  Copyright © 2018 Yohan Lee. All rights reserved.
+//
+
+#include <stdio.h>
+#include "kr1.h"
+
+int main(int argc, const char * argv[]) {
+    //printf("hello world\n");
+    return hello_world();
+}
