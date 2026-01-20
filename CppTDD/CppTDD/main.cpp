0286ddb6d8caa6254ebf86614e25454c03396d00
Yohan Lee <yohanlee@yahoo.com>
2026-01-19 20:52:41 -0500

    initial

---
+++ CppTDD/CppTDD/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppTDD
+//
+//  Created by Yohan Lee on 1/19/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
