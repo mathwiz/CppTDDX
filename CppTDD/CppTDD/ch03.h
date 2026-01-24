9d8187997486a36f83583e5c733d7be45a8193b9
Yohan Lee <yohanlee@yahoo.com>
2026-01-24 12:55:06 -0500

    3 3

--- CppTDD/CppTDD/ch03.h
+++ CppTDD/CppTDD/ch03.h
@@ -15,7 +15,6 @@ void ex3_1() {
 }
 
 
-
 void ex3_2() {
     int gettysburg{1863};
     int *gettysburg_address = &gettysburg;
@@ -26,3 +25,37 @@ void ex3_2() {
     printf("gettysburg_address: %p\n", gettysburg_address);
 }
 
+
+
+struct ClockOfTheLongNow {
+    ClockOfTheLongNow() {
+    }
+    ClockOfTheLongNow(int y) {
+        if(!set_year(y)) {
+            year = DEFAULT * -1;
+        }
+    }
+    void add_year() {
+        year++;
+    }
+    bool set_year(int new_year) {
+        if (new_year < 2026) return false;
+        year = new_year;
+        return true;
+    }
+    int get_year() {
+        return year;
+    }
+private:
+    int DEFAULT = 2000;
+    int year = DEFAULT;
+};
+
+void ex3_3() {
+    ClockOfTheLongNow clock{};
+    ClockOfTheLongNow* clock_ptr = &clock;
+    printf("Clock address: %p\n", clock_ptr);
+    printf("Clock year: %d\n", clock_ptr->get_year());
+    clock_ptr->set_year(2026);
+    printf("Clock year: %d\n", clock_ptr->get_year());
+}
--- CppTDD/CppTDD/main.cpp
+++ CppTDD/CppTDD/main.cpp
@@ -10,7 +10,7 @@
 #include "ch03.h"
 
 int main(int argc, const char * argv[]) {
-    ex3_2();
+    ex3_3();
     std::cout << "Exiting...";
     return EXIT_SUCCESS;
 }
