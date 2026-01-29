2160f8c005bcef866ea358c09a3ef23f04301c20
Yohan Lee <yohanlee@yahoo.com>
2026-01-29 15:38:27 -0500

    4 19

--- CppTDD/CppTDD/ch04.h
+++ CppTDD/CppTDD/ch04.h
@@ -6,7 +6,9 @@
 //
 
 #include <cstdio>
+#include <cstring>
 #include <stdexcept>
+
 
 static int rat_things_power = 200;
 
@@ -126,3 +128,70 @@ void ex4_13() {
         printf("Caught: %s\n", e.what());
     }
 }
+
+
+struct SimpleString {
+    SimpleString(size_t max) :
+    max_size{ max },
+    length{} {
+        if (max_size == 0) {
+            throw std::runtime_error{ "Max size must be at least 1." };
+        }
+        buffer = new char[max_size];
+        buffer[0] = 0;
+    }
+    ~SimpleString() {
+        delete[] buffer;
+    }
+    void print(const char* tag) const {
+        printf("%s: %s", tag, buffer);
+    }
+    bool append_line(const char* x) {
+        const auto x_len = strlen(x);
+        if (x_len + length + 2 > max_size) return false;
+        std::strncpy(buffer + length, x, max_size - length);
+        length += x_len;
+        buffer[length++] = '\n';
+        buffer[length] = 0;
+        return true;
+    }
+private:
+    size_t max_size;
+    char* buffer;
+    size_t length;
+};
+
+void ex4_16() {
+    SimpleString string{ 115 };
+    string.append_line("Starbuck, whaddya hear?");
+    string.append_line("Nothin' but the rain.");
+    string.print("A: ");
+    string.append_line("Grab your gun and bring the cat in.");
+    string.append_line("Aye-aye sir, coming home.");
+    string.print("B: ");
+    if (!string.append_line("Galactic!")) {
+        printf("String was not big enough to append another message.");
+    }
+}
+
+
+struct SimpleStringOwner {
+    SimpleStringOwner(const char* x) :
+    string{ 10 } {
+        if (!string.append_line(x)) {
+            throw std::runtime_error{ "Not enough memory!"};
+        }
+        string.print("Constructed: ");
+    }
+    ~SimpleStringOwner() {
+        string.print("About to destroy: ");
+    }
+private:
+    SimpleString string;
+};
+
+void ex4_19() {
+    SimpleStringOwner x{ "x" };
+    printf("x is alive\n");
+}
+
