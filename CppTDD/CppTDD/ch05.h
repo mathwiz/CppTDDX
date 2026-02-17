95e7b69c5540f1239701d70077cf856ce9bbc5aa
Yohan Lee <yohanlee@yahoo.com>
2026-02-17 15:13:14 -0500

    5

--- CppTDD/CppTDD/ch05.h
+++ CppTDD/CppTDD/ch05.h
@@ -13,19 +13,60 @@
 
 struct ConsoleLogger {
     void log_transaction(long from, long to, double amount) {
-        printf("%ld -> %ld: %f\n", from, to, amount);
+        printf("[cons] %ld -> %ld: %f\n", from, to, amount);
     }
 };
 
-struct Bank {
-    void make_transfer(long from, long to, double amount) {
-        logger.log_transaction(from, to, amount);
+struct FileLogger {
+    void log_transaction(long from, long to, double amount) {
+        printf("[file] %ld,%ld,%f\n", from, to, amount);
     }
-    ConsoleLogger logger;
+};
+
+enum class LoggerType {
+    Console,
+    File
+};
+
+struct Bank {
+    Bank() : type { LoggerType::Console } { }
+    
+    void set_logger(LoggerType new_type) {
+        type = new_type;
+    }
+    
+    void make_transfer(long from, long to, double amount) {
+        switch(type) {
+            case LoggerType::Console: {
+                consoleLogger.log_transaction(from, to, amount);
+                break;
+            }
+            case LoggerType::File: {
+                fileLogger.log_transaction(from, to, amount);
+                break;
+            }
+            default: {
+                throw std::logic_error("Unknown logger type encountered.");
+            }
+        }
+    }
+private:
+    LoggerType type;
+    FileLogger fileLogger;
+    ConsoleLogger consoleLogger;
 };
 
 void ex5_1() {
     Bank bank;
     bank.make_transfer(1000, 2000, 49.95);
     bank.make_transfer(2000, 4000, 20.40);
+}
+
+
+void ex5_2() {
+    Bank bank;
+    bank.make_transfer(1000, 2000, 49.95);
+    bank.make_transfer(2000, 4000, 20.40);
+    bank.set_logger(LoggerType::File);
+    bank.make_transfer(3000, 2000, 75.00);
 }
--- CppTDD/CppTDD/main.cpp
+++ CppTDD/CppTDD/main.cpp
@@ -10,7 +10,7 @@
 #include "ch05.h"
 
 int main(int argc, const char * argv[]) {
-    ex5_1();
+    ex5_2();
     std::cout << "Exiting...";
     return EXIT_SUCCESS;
 }
