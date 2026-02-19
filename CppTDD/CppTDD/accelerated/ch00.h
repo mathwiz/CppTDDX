8dad6e69a73553010ebbe089b3c29b31b23a79af
Yohan Lee <yohanlee@yahoo.com>
2026-02-19 13:56:09 -0500

    folder for accelerated c++

---
+++ CppTDD/CppTDD/accelerated/ch00.h
@@ -0,0 +1,14 @@
+//
+//  hello_world.cpp
+//  CppTDD
+//
+//  Created by Yohan Lee on 2/19/26.
+//
+
+#include <iostream>
+
+int c00_1()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
