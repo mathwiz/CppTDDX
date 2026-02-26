d699cc89414f93b85b0b84eb939273276526d12d
Yohan Lee <yohanlee@yahoo.com>
2026-02-26 13:43:25 -0500

    8 9

--- CppTDD/CppTDD/crash_course/ch08.h
+++ CppTDD/CppTDD/crash_course/ch08.h
@@ -7,6 +7,7 @@
 
 #include <cstdio>
 #include <cstdint>
+#include <stdexcept>
 
 
 void ex8_1() {
@@ -141,5 +142,30 @@ void ex8_8() {
     if (my_specimen == HominidA::afarensis) {
         String linean_name{ "Australopithicus afarensis" };
         printf("My specimen: %s\n", linean_name);
+    }
+}
+
+
+template <typename To, typename From>
+struct NarrowCaster {
+    To cast(From value) {
+        const auto converted = static_cast<To>(value);
+        const auto backwards = static_cast<From>(converted);
+        if (value != backwards) throw std::runtime_error{ "Narrowed!" };
+        return converted;
+    }
+};
+
+template <typename From>
+using short_caster = NarrowCaster<short, From>;
+
+
+void ex8_9() {
+    try {
+        short_caster<int> caster;
+        const auto cyclic_short = caster.cast(142857);
+        printf("cyclic_short: %d\n", cyclic_short);
+    } catch (const std::runtime_error& e) {
+        printf("Exception: %s\n", e.what());
     }
 }
--- CppTDD/CppTDD/main.cpp
+++ CppTDD/CppTDD/main.cpp
@@ -10,7 +10,7 @@
 #include "ch08.h"
 
 int main(int argc, const char * argv[]) {
-    ex8_8();
+    ex8_9();
     std::cout << "Exiting...";
     return EXIT_SUCCESS;
 }
