bce2cb0a8fffec43e2a4da2f753a725ceea52cdb
Yohan Lee <yohanlee@yahoo.com>
2026-04-01 14:30:34 -0400

    13

--- CppTDD/CppTDD/crash_course/ch13.h
+++ CppTDD/CppTDD/crash_course/ch13.h
@@ -28,10 +28,32 @@ void test_static_array() {
     assert_that(local2[4] == 0, "4");
 }
 
+void test_array_access() {
+    std::array<int, 4> fib{ 1, 1, 2, 3 };
+    
+    fib[2] = -1;
+    assert_that(fib[2] == -1, "set with []");
+    
+    fib.at(2) = 2;
+    assert_that(fib[2] == 2, "set with at");
+    try {
+        fib.at(4);
+        assert_that(false, "should not get here");
+    } catch (std::out_of_range e) {
+        assert_that(true, "exception as expected");
+    }
+
+    std::get<2>(fib) = -1;
+    assert_that(fib[2] == -1, "set with get");
+    std::get<2>(fib) = 2;
+    assert_that(fib[2] == 2, "set with get");
+}
+
 void set_up() {
 }
 
 void run_all_tests() {
     run_test(test_static_array, "test_static_array");
+    run_test(test_array_access, "test_array_access");
 }
 
