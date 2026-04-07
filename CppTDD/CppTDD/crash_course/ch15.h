6237b33cf0ae84cd15d859bc48eaca7e38e93077
Yohan Lee <yohanlee@yahoo.com>
2026-04-06 20:50:38 -0400

    15

--- CppTDD/CppTDD/crash_course/ch15.h
+++ CppTDD/CppTDD/crash_course/ch15.h
@@ -5,11 +5,19 @@
 //  Created by Yohan Lee on 4/6/26.
 //
 
+#include <string>
 
 #include "unit_test.h"
 
 // * Testing * //
 
+void test_constructing() {
+    std::string cheese;
+    assert_that(cheese.empty(), "1");
+    std::string motor_club(3, 'A');
+    assert_that(motor_club == "AAA", "2");
+}
+
 void run_all_tests() {
-    printf("todo\n");
+    run_test(test_constructing, "test_constructing");
 }
