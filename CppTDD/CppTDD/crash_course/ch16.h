c7bfad5468ef5fcd0319aee7fafcf5ab6e4f574d
Yohan Lee <yohanlee@yahoo.com>
2026-04-08 12:43:12 -0400

    16

--- CppTDD/CppTDD/crash_course/ch16.h
+++ CppTDD/CppTDD/crash_course/ch16.h
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <iomanip>
 
 #include "unit_test.h"
 
@@ -65,12 +66,29 @@ void test_status() {
     } catch (const exception& e) {
         cerr << "Error occurred reading from stdin: " << e.what();
     }
-    
+
+}
+
+void test_manipulators() {
+    cout << "Gotham needs its " << boolalpha << true << " hero.";
+    cout << "\nMark it " << noboolalpha << false << "!";
+    cout << "\nThere are " << 69 << "," << oct << 105 << " leaves in here.";
+    cout << "\nYabba " << hex << 3669732608 << "!";
+    cout << "\nAvogadro's number: " << scientific << 6.0221415e23;
+    cout << "\nHogwart's platform: " << fixed << setprecision(2) << 9.75123;
+    cout << "\nAlways eliminate " << 3735929054;
+    cout << setw(4) << "\n"
+    << 0x1 << "\n"
+    << 0x10 << "\n"
+    << 0x100 << "\n"
+    << 0x1000
+    << endl;
 }
 
 void run_all_tests() {
     //run_test(test_insertion, "test_insertion");
     //run_test(test_chaining, "test_chaining");
     //run_test(test_extraction, "test_extraction");
-    run_test(test_status, "test_status");
+    //run_test(test_status, "test_status");
+    run_test(test_manipulators, "test_manipulators");
 }
