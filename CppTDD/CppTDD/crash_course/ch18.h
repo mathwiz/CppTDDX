8b0574772f7f9d60863f4149b2af510665d7e8d1
Yohan Lee <yohanlee@yahoo.com>
2026-04-08 20:54:30 -0400

    18

--- CppTDD/CppTDD/crash_course/ch18.h
+++ CppTDD/CppTDD/crash_course/ch18.h
@@ -31,6 +31,22 @@ void test_all_of() {
     assert_that(!all_of(words.cbegin(), words.cend(), len_6_pred), "2");
 }
 
+void test_any_of() {
+    const auto starts_with_b_pred =
+    [](const auto& word) {
+        if (word.empty()) return false;
+        return word[0] == 'B' || word[0] == 'b';
+    };
+    const auto len_6_pred =
+    [](const auto& word) {
+        return word.length() == 6;
+    };
+    
+    assert_that(!any_of(words.cbegin(), words.cend(), starts_with_b_pred), "1");
+    assert_that(any_of(words.cbegin(), words.cend(), len_6_pred), "2");
+}
+
 void run_all_tests() {
+    run_test(test_any_of, "test_any_of");
     run_test(test_all_of, "test_all_of");
 }
